a22ec206623c169efc126e5d47d56fc59892ff2f
Francisco Costa <francisco100eg@gmail.com>
2022-06-14 17:59:08 -0300

    Corrigindo bug da lib aos

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const navCloseEl = document.querySelector('.nav__close');
 const navList = document.querySelector('.nav__list');
 const navIconEl = document.querySelector('.nav__icon');
@@ -25,16 +24,4 @@ navIconEl.addEventListener('click', navOpen);
 
 navCloseEl.addEventListener('click', navClose);
 
-navBgOverlayEl.addEventListener('click', navClose)
-
-
-
-AOS.init({
-    offset:200,
-    delay:100,
-    duration: 600,
-    easing:'ease',
-    once: false,
-    mirror: false,
-    anchorPlacment:'top-bottom'
-});
+navBgOverlayEl.addEventListener('click', navClose);
